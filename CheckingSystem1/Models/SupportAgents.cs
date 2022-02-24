824bde6c8b7499e20ae8566458d74e350de4e3fa
manel talbi <talbimanel20@gmail.com>
2022-02-24 10:18:13 +0100

    email validation

--- CheckingSystem1/Models/SupportAgents.cs
+++ CheckingSystem1/Models/SupportAgents.cs
@@ -14,7 +14,8 @@ namespace CheckingSystem1.Models
         public string LastName { get; set; }
         [Required]
         public string FirstName { get; set; }
-        [Required]
+        [Required(ErrorMessage = "The email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
         public List<Incidents> Incident { get; set; }
     }
